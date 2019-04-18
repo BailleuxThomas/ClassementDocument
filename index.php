7ec6791f7d9abd3a41c79641873293c18e005bd7
Bailleux Thomas <43204476+BailleuxThomas@users.noreply.github.com>
2019-04-18 16:06:25 +0200

    mise a jour

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
 <?php
-$nombre = 0;
-$iterator = new DirectoryIterator('film');
+$nombre = -2;
+$nombre2 = 0;
 
+$dir = "./film";
+//  si le dossier pointe existe
+if (is_dir($dir)) {
+
+   // si il contient quelque chose
+   if ($dh = opendir($dir)) {
+
+       // boucler tant que quelque chose est trouve
+       while (($file = readdir($dh)) !== false) {
+
+
+           // affiche le nom et le type si ce n'est pas un element du systeme
+           if( $file != '.' && $file != '..') {
+            $nombre2++;
+    //    if( $file != '.' && $file != '..' && preg_match('#\.(jpe?g|gif|png)$#i', $file)) { Uniquement photo
+           echo '<a href="film/'.$file.'">'.$file.'</a><br>';
+
+           }
+       }
+                  echo ("Il y a $nombre2 documents <br>");
+       // on ferme la connection
+       closedir($dh);
+   }
+}
+
+
+$iterator = new DirectoryIterator('film');
 // On boucle sur la liste des documents retournés dans l'itérateur
 foreach($iterator as $document){
     $nombre++;
@@ -9,7 +36,6 @@ foreach($iterator as $document){
 }
 echo ('Il y a  '.$nombre.'   documents');
 foreach($iterator as $fichier){
-
     // La fonction isDot retourne TRUE si l'élement courant est "." ou ".."
     if($list_tri== 'DESC'){
         if(!$fichier->isDot()){
